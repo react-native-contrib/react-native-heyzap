93794da0bee4d97a625344c78c5b322c11fc15a8
Hassan Khan <contact@hassankhan.me>
2016-05-30 01:00:08 +0100

    ObjC source is now formatted as per the Clang LLVM style

--- ios/Heyzap.m
+++ ios/Heyzap.m
@@ -4,44 +4,26 @@
 
 RCT_EXPORT_MODULE(Heyzap)
 
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+- (dispatch_queue_t)methodQueue {
+  return dispatch_get_main_queue();
 }
 
-RCT_EXPORT_METHOD(initialize:(NSString *)publisherId)
-{
-    [HeyzapAds startWithPublisherID:publisherId];
+RCT_EXPORT_METHOD(initialize : (NSString *)publisherId) {
+  [HeyzapAds startWithPublisherID:publisherId];
 }
 
-RCT_EXPORT_METHOD(showDebugPanel)
-{
-    [HeyzapAds presentMediationDebugViewController];
+RCT_EXPORT_METHOD(showDebugPanel) {
+  [HeyzapAds presentMediationDebugViewController];
 }
 
-RCT_EXPORT_METHOD(showInterstitialAd)
-{
-    [HZInterstitialAd show];
-}
+RCT_EXPORT_METHOD(showInterstitialAd) { [HZInterstitialAd show]; }
 
-RCT_EXPORT_METHOD(fetchVideoAd)
-{
-    [HZVideoAd fetch];
-}
+RCT_EXPORT_METHOD(fetchVideoAd) { [HZVideoAd fetch]; }
 
-RCT_EXPORT_METHOD(showVideoAd)
-{
-    [HZVideoAd show];
-}
+RCT_EXPORT_METHOD(showVideoAd) { [HZVideoAd show]; }
 
-RCT_EXPORT_METHOD(fetchIncentivizedAd)
-{
-    [HZIncentivizedAd fetch];
-}
+RCT_EXPORT_METHOD(fetchIncentivizedAd) { [HZIncentivizedAd fetch]; }
 
-RCT_EXPORT_METHOD(showIncentivizedAd)
-{
-    [HZIncentivizedAd show];
-}
+RCT_EXPORT_METHOD(showIncentivizedAd) { [HZIncentivizedAd show]; }
 
 @end
