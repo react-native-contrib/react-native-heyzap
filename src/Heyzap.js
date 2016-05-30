09cfbe1382eb24a89c584d66afd8647f033e4cbc
Hassan Khan <contact@hassankhan.me>
2016-05-30 06:52:29 +0100

    Added docblocks for Javascript source

--- src/Heyzap.js
+++ src/Heyzap.js
@@ -4,34 +4,73 @@ let NativeModules = require('NativeModules');
 let NativeHeyzap = NativeModules.Heyzap;
 
 /**
- * High-level docs for the Heyzap API can be written here.
+ * React Native Heyzap module.
+ *
+ * @module react-native-heyzap
  */
-
 let Heyzap = {
+
+    /**
+     * Initializes the Heyzap SDK with a valid publisher ID.
+     *
+     * @param  {string} publisherId
+     *
+     * @return {void}
+     */
     initialize: function(publisherId) {
         NativeHeyzap.initialize(publisherId);
     },
 
+    /**
+     * Shows the Heyzap Mediation Debug screen.
+     *
+     * @return {void}
+     */
     showDebugPanel: function() {
         NativeHeyzap.showDebugPanel();
     },
 
+    /**
+     * Shows an interstitial ad.
+     *
+     * @return {void}
+     */
     showInterstitialAd: function() {
         NativeHeyzap.showInterstitialAd();
     },
 
+    /**
+     * Fetches a video ad.
+     *
+     * @return {void}
+     */
     fetchVideoAd: function() {
         NativeHeyzap.fetchVideoAd();
     },
 
+    /**
+     * Shows a video ad.
+     *
+     * @return {void}
+     */
     showVideoAd: function() {
         NativeHeyzap.showVideoAd();
     },
 
+    /**
+     * Fetches an incentivized ad.
+     *
+     * @return {void}
+     */
     fetchIncentivizedAd: function() {
         NativeHeyzap.fetchIncentivizedAd();
     },
 
+    /**
+     * Shows an incentivized ad.
+     *
+     * @return {void}
+     */
     showIncentivizedAd: function() {
         NativeHeyzap.showIncentivizedAd();
     },
